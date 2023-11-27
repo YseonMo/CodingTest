908cbd7916a6b250c9c48746ff984ebca04e53ea
양선모 <132892991+YseonMo@users.noreply.github.com>
2023-11-27 19:59:40 +0900

    [level unrated] Title: 카운트 업, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.js
@@ -0,0 +1,8 @@
+function solution(start_num, end_num) {
+    let answer = [];
+    let num;
+    for (let num = start_num; num <= end_num; num++){ 
+       answer.push(num)  
+    }
+    return answer;
+}
