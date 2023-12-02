39b514ce476ee023ae6b2b53ab234feb976a3229
양선모 <132892991+YseonMo@users.noreply.github.com>
2023-12-02 21:11:36 +0900

    [level unrated] Title: 점의 위치 구하기, Time: 0.03 ms, Memory: 33 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -0,0 +1,8 @@
+function solution(dot) {
+    let answer = 0;
+    if(dot[0] > 0 && dot[dot.length-1] > 0){answer = 1}
+    else if(dot[0] < 0 && dot[dot.length-1] > 0){answer = 2}
+    else if(dot[0] < 0 && dot[dot.length-1] < 0){answer = 3}
+    else if(dot[0] > 0 && dot[dot.length-1] < 0){answer = 4}
+    return answer;
+}
