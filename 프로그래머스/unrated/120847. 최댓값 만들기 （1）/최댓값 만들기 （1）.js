4c97b7324b5c12df11151889d67e28ec16e1dad4
양선모 <132892991+YseonMo@users.noreply.github.com>
2023-12-03 09:23:30 +0900

    [level unrated] Title: 최댓값 만들기 (1), Time: 3.09 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -0,0 +1,7 @@
+function solution(numbers) {
+    var answer = 0;
+    numbers.sort((a, b) => a - b);
+    console.log(numbers)
+    answer = numbers[numbers.length - 1] * numbers[numbers.length - 2]
+    return answer;
+}
