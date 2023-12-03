291e8f1b875a9ab47f38cd955b825b96e32ba9d7
양선모 <132892991+YseonMo@users.noreply.github.com>
2023-12-03 09:34:19 +0900

    [level unrated] Title: 머쓱이보다 키 큰 사람, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -0,0 +1,10 @@
+function solution(array, height) {
+    var answer = 0;
+    array.sort((a , b) => a - b)
+    for(let i = 0; i < array.length; i++){
+        if(array[i] > height){
+            answer += 1
+        }
+    }
+    return answer;
+}
