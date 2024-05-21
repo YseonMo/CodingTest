6ef68fbca62575c0f9d12def5a17dc52c4b15c19
양선모 <132892991+YseonMo@users.noreply.github.com>
2024-05-21 12:06:22 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 3.19 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,10 @@
+function solution(my_string, num1, num2) {
+
+    let str1 = my_string[num1]
+    let str2 = my_string[num2]
+    let splitArr = my_string.split('')
+    splitArr[num1] = str2
+    splitArr[num2] = str1
+    console.log(splitArr)
+    return splitArr.join('');
+}
