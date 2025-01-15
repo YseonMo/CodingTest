24a634294ce56c929ab7287626400724ad30ab9f
양선모 <132892991+YseonMo@users.noreply.github.com>
2025-01-15 15:56:21 +0900

    [Silver IV] Title: 스택, Time: 164 ms, Memory: 12448 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.js
@@ -0,0 +1,28 @@
+let fs = require('fs')
+let input = fs.readFileSync('/dev/stdin').toString().split('\n')
+
+let stack = []
+let result = []
+
+let collection = input.shift()
+
+for (let i = 0; i < collection; i++) {
+  switch (input[i]) {
+    case 'pop':
+      result.push(stack.pop() || -1)
+      break
+    case 'size':
+      result.push(stack.length)
+      break
+    case 'empty':
+      result.push(stack[0] ? 0 : 1)
+      break
+    case 'top':
+      result.push(stack[stack.length - 1] || -1)
+      break
+    default:
+      stack.push(input[i].split(' ')[1])
+      break
+  }
+}
+console.log(result.join('\n'))
